203cf7fc1fdc9b5cf425a3b2d51697bb41efffee
Brandon Saint-John <saint-john@lbl.gov>
2025-07-31 09:04:11 -0700

    format test_atlus

--- tests/test_atlus.py
+++ tests/test_atlus.py
@@ -1,6 +1,10 @@
 from tfmap import Atlus
 
+
 def test_load():
     atlus = Atlus.from_map_filepath("data/test.map")
 
+    assert len(atlus.image_extent()) == 4
+    assert atlus.image_extent()[0] < atlus.image_extent()[1]
+    assert atlus.image_extent()[2] < atlus.image_extent()[3]
     assert len(atlus.spectra()) == 6732
